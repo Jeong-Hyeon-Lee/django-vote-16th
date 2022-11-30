8b40c361c5bd45d070f840c7b63297a860b22610
geniee44 <68368633+geniee44@users.noreply.github.com>
2022-11-30 22:30:30 +0900

    make: urls

---
+++ finble/vote/urls.py
@@ -0,0 +1,10 @@
+from .views import *
+from django.urls import path
+
+app_name = 'vote'
+
+urlpatterns = [
+    path('results/<str:part>', VoteResult.as_view()),
+    path('join/', JoinView.as_view()),
+    path('login/', LoginView.as_view()),
+]
