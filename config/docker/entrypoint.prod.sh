3cc9e2a282a52a92d3f44a63fa3440dffdcb07a8
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2022-12-17 15:45:28 +0900

    fix static urls

--- config/docker/entrypoint.prod.sh
+++ config/docker/entrypoint.prod.sh
@@ -3,7 +3,7 @@
 cd finble
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations vote
+python manage.py makemigrations
 python manage.py migrate
 
 exec "$@"
--- finble/finble/settings.py
+++ finble/finble/settings.py
@@ -139,8 +139,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'vote.User'
 
